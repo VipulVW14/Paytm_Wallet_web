a75e1b14c92b564fd523dd03780263f458d0f981
Vipul <vipulwasnik0@gmail.com>
2024-09-06 16:02:55 +0530

    feat: adds a TitleTopper ui component

---
+++ packages/ui/src/TitleTopper.tsx
@@ -0,0 +1,7 @@
+"use client"
+export default function TitleTopper({title}: {title: string}){
+    
+    return <div className="text-4xl text-[#6a51a6] w-full my-6 mx-4  font-bold">
+        {title}
+    </div>
+}
